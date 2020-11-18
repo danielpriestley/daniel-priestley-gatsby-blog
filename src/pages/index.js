423df9e0f90f5da837790a3da959a519103307cd
danielpriestley <dannypriestley3@gmail.com>
2020-11-18 13:45:11 +0000

    fixed blog posts sorting, now sorts by recent

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ const IndexPage = ({ data }) => (
 
 export const query = graphql`
 	query {
-		allContentfulBlogPost {
+		allContentfulBlogPost(sort: { fields: [postDate], order: DESC }) {
 			edges {
 				node {
 					postTitle
